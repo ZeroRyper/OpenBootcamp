c6a9517aba160e2e04c267513f406704562d4266
Erick <erick100520@gmail.com>
2023-02-13 16:23:06 -0600

    primer commit

---
+++ Ejercicios tema 3/Primera parte/Primera parte/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primera_parte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultado = 0;
+            resultado=suma(a:4,b:5,c:6);
+            System.Console.WriteLine(resultado);
+        }
+        static int suma(int a,int b,int c)
+        {
+            return a+b+c;
+        }
+    }
+}
---
+++ Ejercicios tema 3/Segunda parte/Segunda parte/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Segunda_parte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro micar= new Carro();
+            micar.puertascarro();
+            Console.WriteLine(micar.puertas);
+
+    }
+    class Carro
+        {
+            public int puertas = 0;
+            public void puertascarro() {
+                this.puertas++;
+            }
+        }
+    }
+}
