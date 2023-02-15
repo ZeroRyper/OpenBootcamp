984c0d0657c1c46d52f3cacad3b59833e49f2f12
Erick <erick100520@gmail.com>
2023-02-15 13:45:31 -0600

    primer commit

---
+++ Introducción a la programación/Ejercicios tema 8/Ejercicios tema 8/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios_tema_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.setedad(22);
+            Console.WriteLine(persona.getedad());
+            persona.setnombre("Erick");
+            Console.WriteLine(persona.getnombre());
+            persona.settelefono(1234567891);
+            Console.WriteLine(persona.gettelefono());
+        }
+    }
+    class Persona
+    {
+        private int edad;
+        private string nombre;
+        private int telefono;
+
+        public void setedad(int edad)
+        {
+            this.edad = edad;
+        }
+        public void setnombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public void settelefono(int telefono)
+        {
+            this.telefono = telefono;
+        }
+        public int getedad()
+        {
+            return this.edad;
+        }
+        public string getnombre()
+        {
+            return this.nombre;
+        }
+        public int gettelefono()
+        {
+            return this.telefono;
+        }
+    }
+}
