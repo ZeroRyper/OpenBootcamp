78a814eb681842fa452c4376031b6836eddc3a85
Erick <erick100520@gmail.com>
2023-02-21 22:19:40 -0600

    primer commit

---
+++ C#/3 Estructuras de Datos/Ejercicio 1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    internal class Program
+    {/*
+    Crea una estructura de datos para un cliente con estos campos:
+    Nombre completo
+    Teléfono
+    Dirección
+    Email
+    Si es nuevo cliente
+    Bonus: escribe un método para presentar estos datos desde la estructura al hacer Console.WriteLine(...)
+      */
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente("Erick",12343456,"Sierra Ai 543","erick100520@gmail.com","Si");
+            Console.WriteLine(cliente.Nombre);
+            Console.WriteLine(cliente.Telefono);
+            Console.WriteLine(cliente.Direccion);
+            Console.WriteLine(cliente.Email);
+            Console.WriteLine(cliente.Escliente);
+        }
+        public struct Cliente {
+            public Cliente(string nombre,int telefono,string direccion,string email,string escliente) { 
+                Nombre=nombre;
+                Telefono = telefono;
+                Direccion = direccion;
+                Email = email;
+                Escliente = escliente;
+
+
+            }
+            public string Nombre { get; }
+            public int Telefono { get; }
+            public string Direccion { get; }
+            public string Email { get; }
+            public string Escliente { get; }
+            public override string ToString() => $"({Telefono})";//Cuando lo escriba lo cambia a string para poder imprimirlo 
+
+        }
+    }
+}
---
+++ C#/3 Estructuras de Datos/Estructuras de Datos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estructuras_de_Datos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Coords miscoordenadas= new Coords(0,12,5);
+            Console.WriteLine(miscoordenadas.X);
+            Console.WriteLine(miscoordenadas.Y);
+            Console.WriteLine(miscoordenadas.Z);
+
+            //Coords miscoordenadas2 = miscoordenadas with { X = 3 };
+        }
+        public struct Coords
+        {
+            public Coords(double x, double y,double z)
+            {
+                X = x;
+                Y = y;
+                Z = z;
+            }
+            public double X { get; }
+            public double Y { get; }
+            public double Z { get; }
+            public override string ToString() => $"({X},{Y},{Z})";//Cuando lo escriba lo cambia a string para poder imprimirlo 
+ 
+        }
+    }
+}
