b0efc5cc9c24eeb89dcc9c877edba6491f1f3664
Erick <erick100520@gmail.com>
2023-02-21 19:14:20 -0600

    primer commit

---
+++ C#/1 Introducción a C#/Ejercicio 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/*
+          Ejercicio 1
+        Escribe un programa que reciba tu 
+        nombre y lo escriba por consola.
+          */
+            Console.Write("Ingrece nombre:");
+            string nombre = Console.ReadLine();
+            Console.WriteLine(nombre);
+        }
+    }
+}
---
+++ C#/1 Introducción a C#/Ejercicio 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa la hora");
+            string hora=Console.ReadLine();//hora es de tipo string para que me permita ingresar (:) y (.)
+            Console.WriteLine(hora);
+
+        }
+    }
+}
