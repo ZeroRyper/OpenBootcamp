05be6a2918de36885ee8ccfb53f45718948a1bb7
Erick <erick100520@gmail.com>
2023-04-23 20:20:55 -0600

    primer commit

---
+++ Curso-Javascript/02-Sintaxis, variables y palabras reservadas de JS/Primitivos.js
@@ -0,0 +1,26 @@
+//Tipos primitivos
+
+//number
+4
+
+//string
+"Hola mundo";
+'Hola mundo';
+`Hola mundo`;
+
+//booleanos
+true;
+false;
+
+//nulo y undefined
+null;
+undefined;
+// null,undefined,false,0 ====>Todas son Falsy
+//null un espacio que esta asignado en memoria pero que esta vacio
+//undefined es un espacio que no esta definido en memoria
+console.log(null === undefined);
+if(true){
+    console.log("cumple");
+}   else{
+    console.log("no cumple");
+}
