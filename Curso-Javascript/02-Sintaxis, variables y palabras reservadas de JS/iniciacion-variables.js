a1800d6ca10c1eb97a4051ead8a1df8a92f267f8
Erick <erick100520@gmail.com>
2023-05-27 22:55:21 -0600

    primer-commit

---
+++ Curso-Javascript/02-Sintaxis, variables y palabras reservadas de JS/iniciacion-variables.js
@@ -0,0 +1,40 @@
+var variable;
+// let variableLet;
+
+//const constante;
+const constante="Hola soy una constante"
+var a=1;
+console.log(a);
+
+a=4;
+console.log(a);
+
+console.log(constante)
+
+let b=3;
+console.log (b)
+
+b=5;
+console.log(b);
+
+var variable="Hola soy una variable VAR"
+
+for (var i=0;i<3;i++){
+    var variable="Soy la segunda variable"
+}
+console.log(variable)
+
+let variableLet="Hola soy una variable LET"
+
+for (var i=0;i<3;i++){
+    let variableLet="Soy la segunda variable LET"
+}
+console.log(variableLet);
+
+var num =4;
+
+console.log(typeof num);
+
+num="Hola soy num";
+
+console.log(typeof num)
