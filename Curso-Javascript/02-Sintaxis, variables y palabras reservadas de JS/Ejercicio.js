6b6cc566624fcd6da6255836dcbe1ebafe246c26
Erick <erick100520@gmail.com>
2023-05-30 00:21:25 -0600

    primer_commit

---
+++ Curso-Javascript/02-Sintaxis, variables y palabras reservadas de JS/Ejercicio.js
@@ -0,0 +1,8 @@
+mi_informacion=["Erick",23,true,Date("may 10 2000"),libro={
+    titulo:"La divina comedia",
+    autor:"Dante",
+    fecha:"1304",
+    url:"https://es.wikipedia.org/wiki/Divina_comedia"
+}]
+
+console.log(mi_informacion)
