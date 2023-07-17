69e223c70b076d7d7164659bb9864930dd698d40
Erick <erick100520@gmail.com>
2023-07-17 16:35:20 -0600

    correcion--multiplicacion

--- Curso-Javascript/10-Módulos en JavaScript/Ejercicio/index.js
+++ Curso-Javascript/10-Módulos en JavaScript/Ejercicio/index.js
@@ -1,8 +1,4 @@
 import {suma,multiplica}  from './modulos/controller.js'
 import chalk from 'chalk';
 
-const sum=suma(4,5)
-console.log(sum)
-
-const mult=multiplica(1,2)
-console.log(chalk.green(mult))
+console.log(chalk.green(multiplica(suma(1, 2), suma(4, 5))))
