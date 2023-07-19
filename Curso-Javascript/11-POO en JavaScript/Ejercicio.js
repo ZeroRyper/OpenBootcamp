ba8f43cc7493ef9c23b819e75c719579146fd970
Erick <erick100520@gmail.com>
2023-07-19 15:58:07 -0600

    correcion

--- Curso-Javascript/11-POO en JavaScript/Ejercicio.js
+++ Curso-Javascript/11-POO en JavaScript/Ejercicio.js
@@ -1,21 +1,13 @@
 class Estudiante{
-constructor(nombre,asignaturas){
-            this.nombre=nombre
-            this.asignaturas=asignaturas
-        }    
-        obtenDatos(){
-            console.log(`Hola, mi nombre es ${this.nombre}, mis asignaturas son ${this.asignaturas}.`)
+    nombre="Erick"
+    asignaturas=["Espanol","Matematicas","Etica"]
+    obtenDatos(){
+        return{
+            nombre:this.nombre,
+            asignaturas:this.asignaturas
         }
-}
-class GradoEst extends Estudiante{
-    constructor(nombre,asignaturas,grado){
-        super(nombre,asignaturas)
-        this.grado=grado
-    }
-    obtenDatos(){// Override
-        super.saludo()
-        console.log(`Grado ${this.grado}`)
     }
 }
-const Alumno=new GradoEst("Gorka",["Espanol","Matematicas","Etica"],"2A")
-console.log(Alumno)
+
+const Alumno=new Estudiante()
+console.log(Alumno.obtenDatos())
